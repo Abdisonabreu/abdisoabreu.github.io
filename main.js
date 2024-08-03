e1d6e2abf4be55c1e7ab2e31725a166c137305c1
Abdisonabreu <abdisonabreu23@gmail.com>
2024-08-03 14:42:11 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+/*
+
+let nombre = prompt('Cual es tu nombre')
+
+let spamElement = document.querySelector(".propuesta-text-cont>span");
+
+if (nombre==null) {}
+else {spamElement.outerHTML = nombre + ", " + spamElement.outerHTML}
+
+*/
